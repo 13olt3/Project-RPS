7045c9532f1ceec4a94bd13d142d8ea3f838bd12
13olt3 <31859433+13olt3@users.noreply.github.com>
2024-11-12 16:24:05 +1100

    Added showBoardState and playerMove functions

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-let user = prompt("Enter name");
-let player1 = tikTakToe(user);
+let user1 = prompt(`Enter name1`);
+let user2 = prompt(`Enter name2`);
 
-function tikTakToe(name){
-    const user = createPlayer(name);
-    let gameBoard = {
-        top: "eee",
-        middle: "eee",
-        bottom: "eee"
-        }
+
+// let firstMove = promt(`choose a square`);
+let game = tikTakToe(user1, user2);
+
+function tikTakToe(name1, name2){
+    const user = createPlayer(name1, name2);
+    let gameBoard = [];
+    for(let i=1;i<10;++i){
+        gameBoard.push(`${i}e`);
+    }
     
     let getBoardState = () => gameBoard;
-    
+    function playerMove(position){
+        let move = overlapCheck(position);
+        gameBoard[move-1] = user.player1Mark();
+    }
+    function overlapCheck(position){
+        if (gameBoard[position-1] == ' x' || gameBoard[position-1] == ' O'){
+            let newMove = prompt('invalid move, choose another move');
+            return newMove;
+        }
+        else return position;
+    }
 
-    
-    return Object.assign({},{getBoardState}, user)
+    function showBoardState(){
+        console.log (`${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}\n${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}\n${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}`)
+    }
+
+    return Object.assign({},{getBoardState, playerMove, showBoardState}, user)
 };
 
-function createPlayer(name){
-    function getName(){
-        return name;
-    }
-    return {getName};
+function createPlayer(name1, name2){
+
+    let getName1 = () => name1;
+    let player1Mark = () => ' x';
+    let getName2 = () => name2;
+    let player2Mark = () => ' O';
+    
+    return {getName1, player1Mark, getName2, player2Mark};
 }
+
 
