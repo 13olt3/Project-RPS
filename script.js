1a6901ce04b964cd427d366dc5a19e5f3964d080
13olt3 <31859433+13olt3@users.noreply.github.com>
2024-11-13 16:56:22 +1100

    Finished console version of RPS

--- script.js
+++ script.js
@@ -11,25 +11,72 @@ function tikTakToe(name1, name2){
     for(let i=1;i<10;++i){
         gameBoard.push(`${i}e`);
     }
+    let winner = 0;
     
     let getBoardState = () => gameBoard;
-    function playerMove(position){
+    function playerMove(position){      
         let move = overlapCheck(position);
-        gameBoard[move-1] = user.player1Mark();
+        gameBoard[move-1] = checkWhoseTurn();
+        winner = winConditionCheck(gameBoard);
+
     }
+    function playGame(){
+        while ((winner != 1) && (winner != 2) && (winner != 3)){
+            let position = prompt(`${showBoardState()} choose move`);
+            playerMove(position);
+        }
+        if (winner == 1){
+            
+            alert (`${user.getName1()} wins!`);
+        }
+        else if (winner == 2){
+            
+            alert(`${user.getName2()} wins!`);
+        }
+        else if (winner == 3){
+            alert(`Draw!`);
+        }
+    }
+
     function overlapCheck(position){
         if (gameBoard[position-1] == ' x' || gameBoard[position-1] == ' O'){
             let newMove = prompt('invalid move, choose another move');
             return newMove;
         }
-        else return position;
+        else {
+            return position;
+        }
     }
+    function wholeGame(){
+        alert(user.getName1());
+    }
+
+    function checkWhoseTurn(){
+        let x = 0;
+        let o = 0;
+        for (let i=0; i<=gameBoard.length;++i){
+            if (gameBoard[i] == ' x'){
+                x++;
+            }
+            else if (gameBoard[i] == ' O'){
+                o++;
+            }
+        }
+        if (x > o){
+            return user.player2Mark();
+        }
+        else {
+            return user.player1Mark();
+        }
+    }
+
+    
 
     function showBoardState(){
         console.log (`${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}\n${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}\n${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}`)
     }
 
-    return Object.assign({},{getBoardState, playerMove, showBoardState}, user)
+    return Object.assign({},{getBoardState, playerMove, showBoardState, wholeGame, playGame}, user)
 };
 
 function createPlayer(name1, name2){
@@ -41,5 +88,85 @@ function createPlayer(name1, name2){
     
     return {getName1, player1Mark, getName2, player2Mark};
 }
+
+function winConditionCheck(gameBoard){
+    if (gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2]){
+        if (gameBoard[0] == " x"){
+            return 1;
+        }
+        else if (gameBoard[0] == " O"){
+            return 2;
+        }
+    }
+    if (gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6]){
+        if (gameBoard[0] == " x"){
+            return 1;
+        }
+        else if (gameBoard[0] == " O"){
+            return 2;
+        }
+    }
+    if (gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5]){
+        if (gameBoard[3] == " x"){
+            return 1;
+        }
+        else if (gameBoard[3] == " O"){
+            return 2;
+        }
+    }
+    if (gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8]){
+        if (gameBoard[6] == " x"){
+            return 1;
+        }
+        else if (gameBoard[6] == " O"){
+            return 2;
+        }
+    }
+    if (gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7]){
+        if (gameBoard[1] == " x"){
+            return 1;
+        }
+        else if (gameBoard[1] == " O"){
+            return 2;
+        }
+    }
+    if (gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8]){
+        if (gameBoard[2] == " x"){
+            return 1;
+        }
+        else if (gameBoard[2] == " O"){
+            return 2;
+        }
+    }
+    if (gameBoard[2] == gameBoard[4] && gameBoard[2] == gameBoard[6]){
+        if (gameBoard[2] == " x"){
+            return 1;
+        }
+        else if (gameBoard[2] == " O"){
+            return 2;
+        }
+    }
+    if (gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8]){
+        if (gameBoard[0] == " x"){
+            return 1;
+        }
+        else if (gameBoard[0] == " O"){
+            return 2;
+        }
+    }
+    let count = 0;
+    for (let i=0; i<gameBoard.length;++i){
+        
+        if ((gameBoard[i]== ' x') || (gameBoard[i] == ' O')){
+            count++;
+        }
+        if (count == 9){
+            return 3;
+        }
+    }
+    
+}
+
+
 
 
