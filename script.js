fe8f74b9628b0564ca7656b1a16ca8af859d4788
13olt3 <31859433+13olt3@users.noreply.github.com>
2024-11-16 18:02:20 +1100

    Added game images, started DOM wiring

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let user1 = prompt(`Enter name1`);
-let user2 = prompt(`Enter name2`);
+let user1 = "tay";
+let user2 = "bob";
 
-
-// let firstMove = promt(`choose a square`);
 let game = tikTakToe(user1, user2);
 
 function tikTakToe(name1, name2){
@@ -11,45 +9,71 @@ function tikTakToe(name1, name2){
     for(let i=1;i<10;++i){
         gameBoard.push(`${i}e`);
     }
+
     let winner = 0;
     
     let getBoardState = () => gameBoard;
     function playerMove(position){      
-        let move = overlapCheck(position);
-        gameBoard[move-1] = checkWhoseTurn();
-        winner = winConditionCheck(gameBoard);
+        let move = position;
+        if (overlapCheck(position) == true){
+            
+            return alert("invalid move");
+        }
+        if (overlapCheck(position) == false){
+            gameBoard[move-1] = checkWhoseTurn();
+            winner = winConditionCheck(gameBoard);
+            if (winner == 1){
+                
+                alert (`${user.getName1()} wins!`);
+            }
+            else if (winner == 2){
+                
+                alert(`${user.getName2()} wins!`);
+            }
+            else if (winner == 3){
+                alert(`Draw!`);
+            }
+        }
 
     }
-    function playGame(){
-        while ((winner != 1) && (winner != 2) && (winner != 3)){
-            let position = prompt(`${showBoardState()} choose move`);
-            playerMove(position);
-        }
-        if (winner == 1){
-            
-            alert (`${user.getName1()} wins!`);
-        }
-        else if (winner == 2){
-            
-            alert(`${user.getName2()} wins!`);
-        }
-        else if (winner == 3){
-            alert(`Draw!`);
+     
+    function resetBoard(){
+        gameBoard = [];
+        for(let i=1;i<10;++i){
+            gameBoard.push(`${i}e`);
         }
     }
+    // function playGame(){
+    //     while ((winner != 1) && (winner != 2) && (winner != 3)){
+    //         let position = prompt(`${showBoardState()} choose move`);
+    //         playerMove(position);
+    //     }
+    //     if (winner == 1){
+            
+    //         alert (`${user.getName1()} wins!`);
+    //     }
+    //     else if (winner == 2){
+            
+    //         alert(`${user.getName2()} wins!`);
+    //     }
+    //     else if (winner == 3){
+    //         alert(`Draw!`);
+    //     }
+    // }
 
     function overlapCheck(position){
         if (gameBoard[position-1] == ' x' || gameBoard[position-1] == ' O'){
-            let newMove = prompt('invalid move, choose another move');
-            return newMove;
+            let checker = true;
+            
+            return true;
         }
         else {
-            return position;
+            let checker = false;
+            
+            return false;
         }
     }
-    function wholeGame(){
-        alert(user.getName1());
-    }
+
 
     function checkWhoseTurn(){
         let x = 0;
@@ -65,9 +89,92 @@ function tikTakToe(name1, name2){
         if (x > o){
             return user.player2Mark();
         }
-        else {
+        else if(o >= x){
             return user.player1Mark();
         }
+    }
+
+    function winConditionCheck(gameBoard){
+        let count = 0;
+
+        if (gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2]){
+            if (gameBoard[0] == " x"){
+                return 1;
+            }
+            else if (gameBoard[0] == " O"){
+                return 2;
+            }
+        }
+        if (gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6]){
+            if (gameBoard[0] == " x"){
+                return 1;
+            }
+            else if (gameBoard[0] == " O"){
+                return 2;
+            }
+        }
+        if (gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5]){
+            if (gameBoard[3] == " x"){
+                return 1;
+            }
+            else if (gameBoard[3] == " O"){
+                return 2;
+            }
+        }
+        if (gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8]){
+            if (gameBoard[6] == " x"){
+                return 1;
+            }
+            else if (gameBoard[6] == " O"){
+                return 2;
+            }
+        }
+        if (gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7]){
+            if (gameBoard[1] == " x"){
+                return 1;
+            }
+            else if (gameBoard[1] == " O"){
+                return 2;
+            }
+        }
+        if (gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8]){
+            if (gameBoard[2] == " x"){
+                return 1;
+            }
+            else if (gameBoard[2] == " O"){
+                return 2;
+            }
+        }
+        if (gameBoard[2] == gameBoard[4] && gameBoard[2] == gameBoard[6]){
+            if (gameBoard[2] == " x"){
+                return 1;
+            }
+            else if (gameBoard[2] == " O"){
+                return 2;
+            }
+        }
+        if (gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8]){
+            if (gameBoard[0] == " x"){
+                return 1;
+            }
+            else if (gameBoard[0] == " O"){
+                return 2;
+            }
+        }
+
+
+        for (let i=0; i<gameBoard.length;++i){
+            
+            if ((gameBoard[i]== ' x') || (gameBoard[i] == ' O')){
+                count++;
+            }
+            if (count == 9){
+                return 3;
+            }
+        }
+
+        return 0;
+        
     }
 
     
@@ -76,7 +183,7 @@ function tikTakToe(name1, name2){
         console.log (`${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]}\n${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]}\n${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}`)
     }
 
-    return Object.assign({},{getBoardState, playerMove, showBoardState, wholeGame, playGame}, user)
+    return Object.assign({},{getBoardState, playerMove, showBoardState, checkWhoseTurn, overlapCheck, winConditionCheck, resetBoard}, user)
 };
 
 function createPlayer(name1, name2){
@@ -89,84 +196,43 @@ function createPlayer(name1, name2){
     return {getName1, player1Mark, getName2, player2Mark};
 }
 
-function winConditionCheck(gameBoard){
-    if (gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2]){
-        if (gameBoard[0] == " x"){
-            return 1;
+
+
+const placeMarker = document.querySelectorAll(".gameBoard > div");
+placeMarker.forEach((square) =>{
+    square.addEventListener('click', function(e){
+        let move = parseInt(e.target.getAttribute("boxNo"));
+        let mark = game.checkWhoseTurn();
+        if ( game.winConditionCheck(game.getBoardState()) != 0){
+            return;
         }
-        else if (gameBoard[0] == " O"){
-            return 2;
+        if (!game.overlapCheck(move)){
+            if (mark == ' x'){
+                e.target.classList.add("cross");
+                e.target.classList.remove("circle");
+            }
+            else if (mark == ' O'){
+                e.target.classList.add("circle");
+                e.target.classList.remove("cross");
+            }
+            game.playerMove(move);
         }
-    }
-    if (gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6]){
-        if (gameBoard[0] == " x"){
-            return 1;
+        else {
+            alert("invalid move");
         }
-        else if (gameBoard[0] == " O"){
-            return 2;
-        }
-    }
-    if (gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5]){
-        if (gameBoard[3] == " x"){
-            return 1;
-        }
-        else if (gameBoard[3] == " O"){
-            return 2;
-        }
-    }
-    if (gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8]){
-        if (gameBoard[6] == " x"){
-            return 1;
-        }
-        else if (gameBoard[6] == " O"){
-            return 2;
-        }
-    }
-    if (gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7]){
-        if (gameBoard[1] == " x"){
-            return 1;
-        }
-        else if (gameBoard[1] == " O"){
-            return 2;
-        }
-    }
-    if (gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8]){
-        if (gameBoard[2] == " x"){
-            return 1;
-        }
-        else if (gameBoard[2] == " O"){
-            return 2;
-        }
-    }
-    if (gameBoard[2] == gameBoard[4] && gameBoard[2] == gameBoard[6]){
-        if (gameBoard[2] == " x"){
-            return 1;
-        }
-        else if (gameBoard[2] == " O"){
-            return 2;
-        }
-    }
-    if (gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8]){
-        if (gameBoard[0] == " x"){
-            return 1;
-        }
-        else if (gameBoard[0] == " O"){
-            return 2;
-        }
-    }
-    let count = 0;
-    for (let i=0; i<gameBoard.length;++i){
         
-        if ((gameBoard[i]== ' x') || (gameBoard[i] == ' O')){
-            count++;
-        }
-        if (count == 9){
-            return 3;
-        }
-    }
-    
-}
+    })
+});
 
+const resetBtn = document.querySelector('.resetGame');
+resetBtn.addEventListener('click', function(){
+    const selectBoard = document.querySelectorAll('.gameBoard > div');
+    selectBoard.forEach((section) =>{
+        section.classList.remove('circle');
+        section.classList.remove('cross');
 
+    })
+    game.resetBoard();
+});
 
 
